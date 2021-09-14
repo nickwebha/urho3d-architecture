f56fe690cd682d0ec346f3e105aea9439b8b2095
Nicholas J Ingrassellino <nick@lifebloodnetworks.com>
2021-09-14 09:38:48 -0400

    Simplified WorldUI a little.

--- source/worldUI.cpp
+++ source/worldUI.cpp
@@ -4,18 +4,6 @@ void WorldUI::HandleCloseButtonPressed( Urho3D::StringHash eventType, Urho3D::Va
 	this->SetMenuOpen( false );
 };
 
-void WorldUI::HandleControlClicked( Urho3D::StringHash eventType, Urho3D::VariantMap& eventData ) {
-	auto* windowTitle = window_->GetChildStaticCast< Urho3D::Text >( "WindowTitle", true );
-
-	auto* clicked = static_cast< Urho3D::UIElement* >( eventData[ Urho3D::UIMouseClick::P_ELEMENT ].GetPtr() );
-
-	Urho3D::String name = "...?";
-	if ( clicked )
-		name = clicked->GetName();
-
-	windowTitle->SetText( "Hello, " + name + "!" );
-};
-
 void WorldUI::HandleButtonPressed( Urho3D::StringHash eventType, Urho3D::VariantMap& eventData ) {
 	this->GetSubsystem< Urho3D::Engine >()->Exit();
 };
@@ -35,7 +23,6 @@ void WorldUI::Start( void ) {
 	this->window_->SetMinWidth( 384 );
 	this->window_->SetLayout( Urho3D::LM_VERTICAL, 6, Urho3D::IntRect( 6, 6, 6, 6 ) );
 	this->window_->SetAlignment( Urho3D::HA_CENTER, Urho3D::VA_CENTER );
-	this->window_->SetName( "window_" );
 
 	auto* titleBar = new Urho3D::UIElement( this->GetContext() );
 	titleBar->SetMinSize( 0, 24 );
@@ -43,11 +30,9 @@ void WorldUI::Start( void ) {
 	titleBar->SetLayoutMode( Urho3D::LM_HORIZONTAL );
 
 	auto* windowTitle = new Urho3D::Text( this->GetContext() );
-	windowTitle->SetName( "WindowTitle" );
-	windowTitle->SetText( "Exit" );
+	windowTitle->SetText( "Exit?" );
 
 	auto* buttonClose = new Urho3D::Button( this->GetContext() );
-	buttonClose->SetName( "CloseButton" );
 
 	titleBar->AddChild( windowTitle );
 	titleBar->AddChild( buttonClose );
@@ -59,12 +44,11 @@ void WorldUI::Start( void ) {
 	buttonClose->SetStyle( "CloseButton" );
 
 	auto* button = new Urho3D::Button( this->GetContext() );
-	button->SetName( "ExitButton" );
 	button->SetMinHeight( 24 );
 
 	auto* buttonText = button->CreateChild< Urho3D::Text >();
-	buttonText->SetFont( cache->GetResource< Urho3D::Font >( "Fonts/BlueHighway.ttf" ), 12 );
-	buttonText->SetAlignment( Urho3D::HA_CENTER, Urho3D::VA_CENTER);
+	buttonText->SetFont( cache->GetResource< Urho3D::Font >( "Fonts/BlueHighway.ttf" ), 14 );
+	buttonText->SetAlignment( Urho3D::HA_CENTER, Urho3D::VA_CENTER );
 	buttonText->SetText( "Exit" );
 
 	this->window_->AddChild( button );
@@ -75,7 +59,6 @@ void WorldUI::Start( void ) {
 
 	this->SubscribeToEvent( buttonClose, Urho3D::E_RELEASED, URHO3D_HANDLER( WorldUI, HandleCloseButtonPressed ) );
 	this->SubscribeToEvent( button, Urho3D::E_RELEASED, URHO3D_HANDLER( WorldUI, HandleButtonPressed ) );
-	this->SubscribeToEvent( Urho3D::E_UIMOUSECLICK, URHO3D_HANDLER( WorldUI, HandleControlClicked ) );
 };
 
 void WorldUI::Stop( void ) {
